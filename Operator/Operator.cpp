78aea91054b71dad97e968df4f73113e969fb48b
Zoha <student@05-03>
2021-12-13 19:19:56 +0200

    add operator /

--- Operator/Operator.cpp
+++ Operator/Operator.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
-
+using namespace std;
+class c_Per
+{
+    int x;
+   // int y;
+public:
+    c_Per()
+    {
+        this->x = 0;
+       // this->y = 0;
+    }
+    c_Per(int x)
+    {
+        this->x = x;
+       // this->y = y;
+    }
+    c_Per operator/(c_Per other)
+    {
+        return c_Per(this->x / other.x);
+    }
+    void print()
+    {
+        cout << this->x;
+    }
+};
 int main()
 {
-    std::cout << "Hello World!\n";
+    c_Per a(20);
+    c_Per b(5);
+    c_Per res = (a / b);
+    
+    res.print();
 }
 
 
